4e2d98f3d54bdad056611eb99c95d7669696681f
Karen Harte <44403544+S00182304@users.noreply.github.com>
2020-05-13 11:41:01 +0100

    Question 6 complete

--- KarenHarte_S00182304/MainWindow.xaml.cs
+++ KarenHarte_S00182304/MainWindow.xaml.cs
@@ -21,13 +21,19 @@ namespace KarenHarte_S00182304
         {
             InitializeComponent();
         }
-
-
-            
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //Loading the phone details
             Phone p1 = new Phone("Samsung S20", 500, "Android", "/images/android.png", "/images/s20.jpg");
             Phone p2 = new Phone("iPhone", 600, "IOS", "/images/apple.png", "/images/iphone11.jpg");
+
+            //Adding the phone details
+            List<Phone> phoneList = new List<Phone>();
+            phoneList.Add(p1);
+            phoneList.Add(p1);
+
+            //Populating them to the list box
+            PhoneDetailsLB.ItemsSource = phoneList;
         }
     }
 }
